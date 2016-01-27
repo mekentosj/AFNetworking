2d58cab2c4605534a5e9349057b64876205da119
mz2 <matias.piipari@gmail.com>
2016-01-27 14:33:19 +0000

    xcodeproj ffs

--- AFNetworking/AFNetworking.h
+++ AFNetworking/AFNetworking.h
@@ -1,43 +1,28 @@
-// AFNetworking.h
 //
-// Copyright (c) 2011 Gowalla (http://gowalla.com/)
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by Matias Piipari on 27/01/2016.
+//  Copyright © 2016 Mekentosj BV. All rights reserved.
+//
 
-#import <Foundation/Foundation.h>
-#import <Availability.h>
+#import <UIKit/UIKit.h>
 
-#ifndef _AFNETWORKING_
-    #define _AFNETWORKING_
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
 
-    #import "AFURLConnectionOperation.h"
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
 
-    #import "AFHTTPRequestOperation.h"
-    #import "AFJSONRequestOperation.h"
-    #import "AFXMLRequestOperation.h"
-    #import "AFPropertyListRequestOperation.h"
-    #import "AFHTTPClient.h"
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking/PublicHeader.h>
 
-    #import "AFImageRequestOperation.h"
-
-    #if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
-        #import "AFNetworkActivityIndicatorManager.h"
-        #import "UIImageView+AFNetworking.h"
-    #endif
-#endif /* _AFNETWORKING_ */
+#import "AFHTTPClient.h"
+#import "AFHTTPClientParameterEncoding.h"
+#import "AFHTTPRequestOperation.h"
+#import "AFImageRequestOperation.h"
+#import "AFJSONRequestOperation.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "AFPropertyListRequestOperation.h"
+#import "AFURLConnectionOperation.h"
+#import "AFXMLRequestOperation.h"
+#import "UIImageView+AFNetworking.h"
