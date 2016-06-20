93dcdcbf105226dc6cfb4b3a75ee8ddc3df21a3f
Ben Blackburne <bpb@lysozyme.net>
2016-06-20 11:52:39 +0100

    Make macOS version

---
+++ AFNetworking macOS/AFNetworking.h
@@ -0,0 +1,27 @@
+//
+//  AFNetworking macOS.h
+//  AFNetworking macOS
+//
+//  Created by Ben Blackburne on 20/06/2016.
+//  Copyright © 2016 Mekentosj BV. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AFNetworking macOS.
+FOUNDATION_EXPORT double AFNetworking_macOSVersionNumber;
+
+//! Project version string for AFNetworking macOS.
+FOUNDATION_EXPORT const unsigned char AFNetworking_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworking_macOS/PublicHeader.h>
+
+#import "AFHTTPClient.h"
+#import "AFHTTPClientParameterEncoding.h"
+#import "AFHTTPRequestOperation.h"
+#import "AFImageRequestOperation.h"
+#import "AFJSONRequestOperation.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "AFPropertyListRequestOperation.h"
+#import "AFURLConnectionOperation.h"
+#import "AFXMLRequestOperation.h"
